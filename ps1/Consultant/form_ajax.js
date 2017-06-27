713e40a1bd9853bb1e3cd6c05549776fb0711de9
Varun Agarwal <agarwal.varun21398@gmail.com>
2017-06-27 12:01:57 +0530

    Delete form_ajax.js

--- ps1/Consultant/form_ajax.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-	   var $form = $('form');
-	   $form.submit(function(){
-	      $.post($(this).attr('action'), $(this).serialize(), function(response){
-	            $.alert("Success");
-	      },'json');
-	      return false;
-	   });
-	});
